789f4b27174020a0b5309745b4c4c06ff5ca9bb3
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-27 13:22:21 -0500

    getting users dynamically

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+
+async function main(){
+  
+    const userWrapper = document.querySelector('.user-list');
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    const userData = await users.json();
+
+    const userHTML = userData.map((element) => userHTMLData(element)).join("")
+
+
+    userWrapper.innerHTML = userHTML
+}
+
+async function ShowUserPost(userId){
+    localStorage.setItem("userId",userId)
+    window.location.href = `${window.location.origin}/user.html`
+  const postLink = "https://jsonplaceholder.typicode.com/posts?userId="+userId
+  console.log(postLink)
+const usersPost = await fetch(postLink)
+console.log(usersPost)
+const usersPostData = await usersPost.json()
+console.log(usersPostData)
+}
+
+main()
+function userHTMLData(data){
+    return `  
+<div class="user" onclick="ShowUserPost(${data.id})">
+    <div class="user-card">
+      <div class="user-card__container">
+        <h3>${data.name}</h4>
+          <p><b>Email:</b> ${data.email}</p>
+          <p><b>Phone:</b> ${data.phone}</p>
+          <p><b>Website:</b> <a href="https://${data.website}" target="_blank">${data.website}</a></p>
+      </div>
+    </div></div>
+  `
+}
+
